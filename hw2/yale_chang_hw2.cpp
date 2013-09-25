a165fe3023e56c54ec92bd300ad8a078849485d4
andybaoxv <changyale123@gmail.com>
2013-09-25 17:40:49 -0400

    Complete Insertion Sort

--- hw2/yale_chang_hw2.cpp
+++ hw2/yale_chang_hw2.cpp
@@ -1,5 +1,5 @@
 // Author: Yale CHANG
-// Platform: g++ on Ubuntu 12.04 LTS
+// Platform: g++(2011) on Ubuntu 12.04 LTS
 
 #include<iostream>
 #include<vector>
@@ -12,4 +12,29 @@ void printVector(vector<int> A);
 int main()
 {
     cout<<"Hello World!"<<endl;
+    int test_vector[] = {3,7,2,9,5,4};
+    vector<int> myvector(test_vector,test_vector+sizeof(test_vector)/sizeof(int));
+    insertionSort(myvector);
+    for(vector<int>::iterator i=myvector.begin();i!=myvector.end();i++)
+        cout<<*i<<",";
+    cout<<endl;
 }
+
+// Implementation of Insertion Sort
+void insertionSort(vector<int> &A)
+{
+    for(vector<int>::iterator j=A.begin()+1;j!=A.end();j++)
+    {
+        vector<int>::value_type key = *j;
+        // Insert A[j] into the sorted sequence A[1..j-1].
+        vector<int>::iterator i = j-1;
+        while(i>=A.begin() && *i>key)
+        {
+            *(i+1) = *i;
+            i--;
+        }
+        *(i+1) = key;
+    }
+}
+
+
