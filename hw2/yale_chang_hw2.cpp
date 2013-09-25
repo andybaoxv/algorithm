cc4a6c5848c2a6e98d16c1cbd9256462355e47d3
andybaoxv <changyale123@gmail.com>
2013-09-25 15:52:53 -0400

    hw2 init

---
+++ hw2/yale_chang_hw2.cpp
@@ -0,0 +1,15 @@
+// Author: Yale CHANG
+// Platform: g++ on Ubuntu 12.04 LTS
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void insertionSort(vector<int> &A );
+void populateVecotr(vector<int> &A, double m);
+void printVector(vector<int> A);
+
+int main()
+{
+    cout<<"Hello World!"<<endl;
+}
